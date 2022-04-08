dcece0fb5afef5d540e43ad0077410461d04cc33
mingxingsun <641464521@qq.com>
2022-04-09 00:35:30 +0800

    11

---
+++ diqi/src/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println(1111);
+        System.out.println(1111);
+        System.out.println(1111);
+        System.out.println(1111);
+        System.out.println(1111);
+    }
+}
