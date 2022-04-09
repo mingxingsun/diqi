9e0e3debfb11431b0b717af3203bfa21aa5f3704
mingxingsun <641464521@qq.com>
2022-04-09 11:37:07 +0800

    调整代码

--- diqi/src/hello.java
+++ diqi/src/hello.java
@@ -6,9 +6,17 @@ public class hello {
         System.out.println(1111);
         System.out.println(1111);
         System.out.println(11112);
-        System.out.println(222222222);
         System.out.println(444444);
+        me();
         System.out.println(22222323);
+
+        int m = 3;
+        System.out.println(m);
+
+    }
+
+    private static void me() {
+        System.out.println(222222222);
         System.out.println("dev修改代码");
     }
 }
