0525059861750deb65f3ee2ea14dd8bb1b055583
mingxingsun <641464521@qq.com>
2022-04-09 13:28:14 +0800

    测试快捷键

---
+++ diqi/src/com/java/HelloWolrd.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class HelloWolrd {
+    public static void main(String[] args) {
+        sun();
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+
+    private static void sun() {
+        for (int j = 1; j < 10; j++) {
+            System.out.println(2222);
+            System.out.println(2222);
+
+        }
+    }
+}
---
+++ diqi/src/com/java/Teacher.java
@@ -0,0 +1,8 @@
+package com.java;
+
+public class Teacher extends Student {
+    public Teacher() {
+        System.out.println("1111");
+    }
+
+}
